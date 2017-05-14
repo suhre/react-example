31f776ffa62aedac8087fe534cae3b21a6e73ef1
Marco Suhre <marco.suhre@googlemail.com>
2017-05-14 23:46:16 +0200

    Create dummy profile card in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-6 col-md-6">
+                            <div className="well well-sm">
+                                <div className="row">
+                                    <div className="col-sm-6 col-md-4">
+                                        <div className="prefix-user-image">
+                                            <i className="fa fa-5x fa-user-circle-o"></i>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6 col-md-8 prefix-profile-details">
+                                        <h4>John Doe</h4>
+                                        <small className="text-muted">
+                                            <cite title="">Berlin, Germany <i
+                                                className="fa fa-map-marker"></i>
+                                            </cite>
+                                        </small>
+                                        <p>
+                                            <i className="fa fa-envelope"></i>email@berlin.com
+                                            <br />
+                                            <i className="fa fa-globe"></i>
+                                            <a href="#">www.berlin.de</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
