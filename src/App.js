7450d62eaca8d6c553075a74c75de0bf72f98563
Marco Suhre <marco.suhre@googlemail.com>
2017-05-21 20:18:32 +0200

    Add the bootstrap jumbotron-narrow example

--- src/App.js
+++ src/App.js
@@ -2,44 +2,81 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {}
+        };
+    }
+
+    loadUserProfileById(id) {
+        this.props.serverConnection.loadUserProfileById(id)
+            .then(function (json) {
+                this.setState({
+                    user: json
+                });
+            }.bind(this));
+    }
+
+    componentDidMount() {
+        //this.loadUserProfileById(1);
+    }
+
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-
+            <div className="container">
+                <div className="header clearfix">
+                    <nav>
+                        <ul className="nav nav-pills pull-right">
+                            <li role="presentation" className="active"><a href="#">Home</a></li>
+                            <li role="presentation"><a href="#">About</a></li>
+                            <li role="presentation"><a href="#">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <h3 className="text-muted">Project name</h3>
                 </div>
 
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xs-12 col-sm-6 col-md-6">
-                            <div className="well well-sm">
-                                <div className="row">
-                                    <div className="col-sm-6 col-md-4">
-                                        <div className="prefix-user-image">
-                                            <i className="fa fa-5x fa-user-circle-o"></i>
-                                        </div>
-                                    </div>
-                                    <div className="col-sm-6 col-md-8 prefix-profile-details">
-                                        <h4>John Doe</h4>
-                                        <small className="text-muted">
-                                            <cite title="">Berlin, Germany <i
-                                                className="fa fa-map-marker"></i>
-                                            </cite>
-                                        </small>
-                                        <p>
-                                            <i className="fa fa-envelope"></i>email@berlin.com
-                                            <br />
-                                            <i className="fa fa-globe"></i>
-                                            <a href="#">www.berlin.de</a>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div className="jumbotron">
+                    <h1>Jumbotron heading</h1>
+                    <p className="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus,
+                        tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet
+                        risus.</p>
+                    <p><a className="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+                </div>
+
+                <div className="row marketing">
+                    <div className="col-lg-6">
+                        <h4>Subheading</h4>
+                        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                        <h4>Subheading</h4>
+                        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus
+                            sit amet fermentum.</p>
+
+                        <h4>Subheading</h4>
+                        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                    </div>
+
+                    <div className="col-lg-6">
+                        <h4>Subheading</h4>
+                        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                        <h4>Subheading</h4>
+                        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus
+                            sit amet fermentum.</p>
+
+                        <h4>Subheading</h4>
+                        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
                     </div>
                 </div>
 
+                <footer className="footer">
+                    <p>&copy; 2016 Company, Inc.</p>
+                </footer>
+
             </div>
+
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import './jumbotron-narrow.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
