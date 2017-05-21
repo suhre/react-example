33bf61f18b8bde6eefc8328fc854793c21b227e8
Marco Suhre <marco.suhre@googlemail.com>
2017-05-21 20:22:05 +0200

    Inject ServerConnection into the <App /> component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import './jumbotron-narrow.css';
+import ServerConnection from './ServerConnection';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import './index.css';
+import './jumbotron-narrow.css';
+
+let serverConnection = new ServerConnection();
 
 ReactDOM.render(
-    <App />,
+    <App serverConnection={serverConnection} />,
     document.getElementById('root')
 );
