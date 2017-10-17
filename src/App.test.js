2daacc985264e19254fc8f12604158885a9a4ca7
Marco Suhre <marco.suhre@googlemail.com>
2017-10-17 13:01:32 +0200

    Add comment

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//TODO: Testing
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
